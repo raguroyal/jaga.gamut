90ab18e59b643653194e4ac29ed5f3bca82a3840
raguraoyal <ragudevops@gmail.com>
2018-07-20 12:04:42 +0530

    this is trail commit

---
+++ jaga.gamut/src/main/java/com/jaga/gamut/App.java
@@ -0,0 +1,13 @@
+package com.jaga.gamut;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
